72cb8d926785984e4f735d29fd76fcf80473a9b0
Sumit Kumar <itsme.sumit96@gmail.com>
2025-05-27 22:30:36 +0530

    fix pin attachments update

--- src/pages/admin/vessel/addEditInventoryPointDrawer.jsx
+++ src/pages/admin/vessel/addEditInventoryPointDrawer.jsx
@@ -222,10 +222,15 @@ const AddEditInventoryPointDrawer = ({ onClose = () => {} }) => {
     );
   };
 
+  console.log("Attachments:", attachments);
+  console.log("Images:", images);
+  console.log("Deleted Images:", deletedImages);
+  console.log("Deleted Attachments:", deletedAttachments);
+
   // Add Attachment Delete Handler
   const handleAttachmentDelete = (file) => {
     if (file.status === "Uploaded") {
-      setDeletedAttachments((prev) => [...prev, file.id]);
+      setDeletedAttachments((prev) => [...prev, file]);
     }
     setAttachments((prev) => prev.filter((att) => att.id !== file.id));
   };
